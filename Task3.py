fa19e3cc1792d231349292eb4e8b6b85254bd7c3
gothamc5 <gouthamkumar.h@gmail.com>
2021-11-17 13:08:42 +0000

    Task 3 completed

--- Task3.py
+++ Task3.py
@@ -1,12 +1,16 @@
-#Task 3 – CPU Problem Diagnoser
+# Task 3 – CPU Problem Diagnoser
 
 import psutil
 import time
 
-historyUsage = list()
-checkInterval = 5
-historyDuration = 120
+cpuUsageHistory = list()
+processCountHistory = list()
+problemRootCauseFound = False
+checkInterval = 2
+historyDuration = 10
 warningThreshold = 90
+cpuCount = psutil.cpu_count()
+currentProcessCount = 0
 
 
 def average(l):
@@ -15,16 +19,48 @@ def average(l):
 
 x = historyDuration
 while x > 0:
-    historyUsage.append(psutil.cpu_percent())
-    time.sleep(checkInterval)
+    cpuUsageHistory.append(psutil.cpu_percent(interval=checkInterval))
+    processCountHistory.append(len(psutil.pids()))
     x -= checkInterval
 
 while True:
-    currentCpuUsage = psutil.cpu_percent()
-    if currentCpuUsage > warningThreshold:
-        print('Warning: CPU usage is High and above 90%')
-    if currentCpuUsage > 2 * average(historyUsage):
-        print('Warning: CPU usage is High than normal levels')
-    historyUsage.pop(0)
-    historyUsage.append(currentCpuUsage)
+    for process in psutil.process_iter():
+        cpu_usage = process.cpu_percent()
     time.sleep(checkInterval)
+    currentCpuUsage = psutil.cpu_percent()
+    if currentCpuUsage > warningThreshold or currentCpuUsage > 2 * average(cpuUsageHistory):
+        print('Warning: CPU usage is High')
+        for process in psutil.process_iter():
+            pid = process.pid
+            cpu_usage = process.cpu_percent()
+            if pid == 0:
+                continue
+            try:
+                coresUsed = len(process.cpu_affinity())
+            except psutil.AccessDenied:
+                coresUsed = 0
+                continue
+            if cpu_usage / cpuCount > currentCpuUsage / 2:
+                print('Root Cause: One particular process is taking more than half of CPU usage')
+                print(process.cpu_affinity())
+                print('            ID        :' + str(pid))
+                print('            Name      :' + str(process.name()))
+                print('            Cores Used:' + str(coresUsed))
+                print('            CPU Used  :' + str(cpu_usage / coresUsed))
+                print('--------------------------------')
+                problemRootCauseFound = True
+        currentProcessCount = len(psutil.pids())
+        if currentProcessCount > 2 * average(processCountHistory):
+            print('Root Cause: There are lot of new process running than usual')
+            print('            Average process count: {}'.format(average(processCountHistory)))
+            print('            Current process count: {}'.format(currentProcessCount))
+            problemRootCauseFound = True
+        if not problemRootCauseFound:
+            print('Root Cause: CPU Usage is High for unknown reason')
+            problemRootCauseFound = False
+    cpuUsageHistory.pop(0)
+    cpuUsageHistory.append(currentCpuUsage)
+    processCountHistory.pop(0)
+    processCountHistory.append(len(psutil.pids()))
+    print(cpuUsageHistory)
+    print(processCountHistory)
--- cpu-test.py
+++ cpu-test.py
@@ -2,16 +2,13 @@ import time
 
 import psutil
 
-# print(psutil.cpu_percent())
+cpuCount = psutil.cpu_count()
+print(psutil.cpu_percent())
 # print(psutil.pids())
 p = psutil.Process(6076)
 # print(p)
-
 while True:
-    print(psutil.cpu_count())
-    # print(p.num_fds())
-    # print(p.cpu_num())
-    print('ID = {}  CPU = {}'.format(p.pid, p.cpu_percent()/psutil.cpu_count()))
+    print('ID = {}  CPU = {}'.format(p.pid, p.cpu_percent()/cpuCount))
     time.sleep(1)
 
 # for i in range(3):

bc4694a2522aa6be2de38590f75bfef2f3690f19
gothamc5 <gouthamkumar.h@gmail.com>
2021-11-17 18:26:25 +0000

    Task5 refactored Task6 Completed

--- Task5.py
+++ Task5.py
@@ -4,6 +4,9 @@ import socket
 portsCheckList = [22, 23, 53, 80, 443]
 hostIP = '127.0.0.1'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('----------------------------------------------------------------------------------')
+print('Port State Report for Machine {}'.format(hostIP))
+print('----------------------------------------------------------------------------------')
 for checkPort in portsCheckList:
     portStatus = sock.connect_ex((hostIP, checkPort))
     if portStatus == 0:
@@ -11,3 +14,4 @@ for checkPort in portsCheckList:
     else:
         print('Port {} is Closed'.format(checkPort))
 sock.close()
+print('----------------------------------------------------------------------------------')
---
+++ Task6.py
@@ -0,0 +1,37 @@
+# Task 6 – Connection Status Checker
+import requests
+
+testUrlList = ['http://www.google.com', 'https://en.wikipedia.org/wiki/Main_Page']
+responseTimesList = list()
+status = 'NO Internet or Very Bad Network'
+
+
+def average(listToAvg):
+    return sum(listToAvg) / len(listToAvg)
+
+
+print('----------------------------------------------------------------------------------')
+print('Running Connection Status Checker on this computer')
+print('----------------------------------------------------------------------------------')
+for url in testUrlList:
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print('Connection Error while connecting website: {}'.format(url))
+        continue
+    except:
+        print('Unknown Error while connecting website: {}'.format(url))
+        continue
+    responseTimesList.append(response.elapsed.total_seconds())
+    print('{} secs elapsed to connect Website: {}'.format(response.elapsed.total_seconds(), url))
+if responseTimesList:
+    avgResponseTime = average(responseTimesList)
+    print('Average response time of all connected websites: {} secs'.format(avgResponseTime))
+    if avgResponseTime <= 0.1:
+        status = 'Very Good Speed'
+    elif avgResponseTime <= 1.0:
+        status = 'Moderate Speed'
+    else:
+        status = 'Bad Speed'
+print('Internet status of this computer: {}'.format(status))
+print('----------------------------------------------------------------------------------')
